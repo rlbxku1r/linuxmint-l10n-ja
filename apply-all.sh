7ef8fad8753e04374c9f007909e081b167db9a09
rlbxku1r <32811754+rlbxku1r@users.noreply.github.com>
2025-08-29 22:43:17 +0900

    Initial commit for Linux Mint 22.3

---
+++ apply-all.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+
+# /usr/share/locale/ja/LC_MESSAGES/*.mo
+msgfmt aptkit_aptkit-ja.po -o /usr/share/locale/ja/LC_MESSAGES/aptkit.mo
+msgfmt bulky_bulky-ja.po -o /usr/share/locale/ja/LC_MESSAGES/bulky.mo
+msgfmt captain_captain-ja.po -o /usr/share/locale/ja/LC_MESSAGES/captain.mo
+msgfmt cinnamon-control-center_cinnamon-control-center-ja.po -o /usr/share/locale/ja/LC_MESSAGES/cinnamon-control-center.mo
+msgfmt cinnamon-screensaver_cinnamon-screensaver-ja.po -o /usr/share/locale/ja/LC_MESSAGES/cinnamon-screensaver.mo
+msgfmt cinnamon-session_cinnamon-session-ja.po -o /usr/share/locale/ja/LC_MESSAGES/cinnamon-session.mo
+msgfmt cinnamon-settings-daemon_cinnamon-settings-daemon-ja.po -o /usr/share/locale/ja/LC_MESSAGES/cinnamon-settings-daemon.mo
+msgfmt cinnamon_cinnamon-ja.po -o /usr/share/locale/ja/LC_MESSAGES/cinnamon.mo
+msgfmt fingwit_fingwit-ja.po -o /usr/share/locale/ja/LC_MESSAGES/fingwit.mo
+msgfmt folder-color-switcher_folder-color-switcher-ja.po -o /usr/share/locale/ja/LC_MESSAGES/folder-color-switcher.mo
+msgfmt hypnotix_hypnotix-ja.po -o /usr/share/locale/ja/LC_MESSAGES/hypnotix.mo
+msgfmt lightdm-settings_lightdm-settings-ja.po -o /usr/share/locale/ja/LC_MESSAGES/lightdm-settings.mo
+msgfmt mintdrivers_mintdrivers-ja.po -o /usr/share/locale/ja/LC_MESSAGES/mintdrivers.mo
+msgfmt mintreport_mintreport-ja.po -o /usr/share/locale/ja/LC_MESSAGES/mintreport.mo
+msgfmt mintupdate_mintupdate-ja.po -o /usr/share/locale/ja/LC_MESSAGES/mintupdate.mo
+msgfmt mintupgrade_mintupgrade-ja.po -o /usr/share/locale/ja/LC_MESSAGES/mintupgrade.mo
+msgfmt nemo_nemo-ja.po -o /usr/share/locale/ja/LC_MESSAGES/nemo.mo
+msgfmt nemo-extensions_nemo-extensions-ja.po -o /usr/share/locale/ja/LC_MESSAGES/nemo-extensions.mo
+msgfmt nvidia-prime-applet_nvidia-prime-applet-ja.po -o /usr/share/locale/ja/LC_MESSAGES/nvidia-prime-applet.mo
+msgfmt pix_pix-ja.po -o /usr/share/locale/ja/LC_MESSAGES/pix.mo
+msgfmt slick-greeter_slick-greeter-ja.po -o /usr/share/locale/ja/LC_MESSAGES/slick-greeter.mo
+msgfmt sticky_sticky-ja.po -o /usr/share/locale/ja/LC_MESSAGES/sticky.mo
+msgfmt thingy_thingy-ja.po -o /usr/share/locale/ja/LC_MESSAGES/thingy.mo
+msgfmt timeshift_timeshift-ja.po -o /usr/share/locale/ja/LC_MESSAGES/timeshift.mo
+msgfmt warpinator_warpinator-ja.po -o /usr/share/locale/ja/LC_MESSAGES/warpinator.mo
+msgfmt webapp-manager_webapp-manager-ja.po -o /usr/share/locale/ja/LC_MESSAGES/webapp-manager.mo
+msgfmt xapp_xapp-ja.po -o /usr/share/locale/ja/LC_MESSAGES/xapp.mo
+msgfmt xedit_xed-ja.po -o /usr/share/locale/ja/LC_MESSAGES/xed.mo
+msgfmt xreader_xreader-ja.po -o /usr/share/locale/ja/LC_MESSAGES/xreader.mo
+msgfmt xviewer_xviewer-ja.po -o /usr/share/locale/ja/LC_MESSAGES/xviewer.mo
+
+chown root:root /usr/share/locale/ja/LC_MESSAGES/*.mo
+chmod a=r,u+w /usr/share/locale/ja/LC_MESSAGES/*.mo
+
+# /usr/share/linuxmint/locale/ja/LC_MESSAGES/*.mo
+msgfmt mint-common_mint-common-ja.po -o /usr/share/linuxmint/locale/ja/LC_MESSAGES/mint-common.mo
+msgfmt mintbackup_mintbackup-ja.po -o /usr/share/linuxmint/locale/ja/LC_MESSAGES/mintbackup.mo
+msgfmt mintdesktop_mintdesktop-ja.po -o /usr/share/linuxmint/locale/ja/LC_MESSAGES/mintdesktop.mo
+msgfmt mintinstall_mintinstall-ja.po -o /usr/share/linuxmint/locale/ja/LC_MESSAGES/mintinstall.mo
+msgfmt mintlocale_mintlocale-ja.po -o /usr/share/linuxmint/locale/ja/LC_MESSAGES/mintlocale.mo
+msgfmt mintmenu_mintmenu-ja.po -o /usr/share/linuxmint/locale/ja/LC_MESSAGES/mintmenu.mo
+msgfmt mintnanny_mintnanny-ja.po -o /usr/share/linuxmint/locale/ja/LC_MESSAGES/mintnanny.mo
+msgfmt mintsources_mintsources-ja.po -o /usr/share/linuxmint/locale/ja/LC_MESSAGES/mintsources.mo
+msgfmt mintstick_mintstick-ja.po -o /usr/share/linuxmint/locale/ja/LC_MESSAGES/mintstick.mo
+msgfmt mintupload_mintupload-ja.po -o /usr/share/linuxmint/locale/ja/LC_MESSAGES/mintupload.mo
+msgfmt mintwelcome_mintwelcome-ja.po -o /usr/share/linuxmint/locale/ja/LC_MESSAGES/mintwelcome.mo
+
+chown root:root /usr/share/linuxmint/locale/ja/LC_MESSAGES/*.mo
+chmod a=r,u+w /usr/share/linuxmint/locale/ja/LC_MESSAGES/*.mo
