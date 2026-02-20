44a9fe8fc1160d5abc9c35f2d3f0474a72a83b22
rlbxku1r <32811754+rlbxku1r@users.noreply.github.com>
2026-02-20 16:20:22 +0900

    apply-all.fish: tweak the handling of xed

--- apply-all.fish
+++ apply-all.fish
@@ -15,6 +15,24 @@ function generate_mo -a PO MO
     echo $MO
 end
 
+function po_name -a CATALOG
+    # If the $CATALOG contains an underscore, use it as a prefix as is.
+    # Otherwise, make a prefix that repeats the $CATALOG twice, separated by an underscore.
+    if string match -q '*_*' $CATALOG
+        # e.g. "xedit_xed" becomes "xedit_xed-$CATALOG_LANG.po".
+        echo $CATALOG-$CATALOG_LANG.po
+    else
+        # e.g. "cinnamon" becomes "cinnamon_cinnamon-$CATALOG_LANG.po".
+        echo "$CATALOG"_$CATALOG-$CATALOG_LANG.po
+    end
+end
+
+function mo_name -a CATALOG
+    # If the $CATALOG is separated by underscores, print its last word. Otherwise, just print it as is.
+    # e.g. "xedit_xed" becomes "xed.mo", and "cinnamon" becomes "cinnamon.mo".
+    echo (string split _ $CATALOG | tail -n1).mo
+end
+
 # /usr/share/locale/$CATALOG_LANG/LC_MESSAGES/*.mo
 set CATALOGS \
     aptkit \
@@ -46,14 +64,13 @@ set CATALOGS \
     warpinator \
     webapp-manager \
     xapp \
+    xedit_xed \
     xreader \
     xviewer
 
 for CATALOG in $CATALOGS
-    generate_mo $CATALOG_DIR/{$CATALOG}_$CATALOG-$CATALOG_LANG.po /usr/share/locale/$CATALOG_LANG/LC_MESSAGES/$CATALOG.mo
+    generate_mo $CATALOG_DIR/(po_name $CATALOG) /usr/share/locale/$CATALOG_LANG/LC_MESSAGES/(mo_name $CATALOG)
 end
-
-generate_mo $CATALOG_DIR/xedit_xed-$CATALOG_LANG.po /usr/share/locale/$CATALOG_LANG/LC_MESSAGES/xed.mo
 
 # /usr/share/linuxmint/locale/$CATALOG_LANG/LC_MESSAGES/*.mo
 set CATALOGS \
@@ -70,7 +87,7 @@ set CATALOGS \
     mintwelcome
 
 for CATALOG in $CATALOGS
-    generate_mo $CATALOG_DIR/{$CATALOG}_$CATALOG-$CATALOG_LANG.po /usr/share/linuxmint/locale/$CATALOG_LANG/LC_MESSAGES/$CATALOG.mo
+    generate_mo $CATALOG_DIR/(po_name $CATALOG) /usr/share/linuxmint/locale/$CATALOG_LANG/LC_MESSAGES/(mo_name $CATALOG)
 end
 
 exit 0
